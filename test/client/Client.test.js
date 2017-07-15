351c7659e4d413ba4b3feb092cf8f78694213166
Bill Robitske, Jr <bill.robitske.jr@gmail.com>
2017-07-14 20:39:40 -0500

    First attempt at front-end testing
    PhantomJS ... "I don't get it..."

---
+++ test/client/Client.test.js
@@ -0,0 +1,31 @@
+// import 'phantomjs';
+
+// import 'mocha';
+// import { expect } from 'chai';
+// import * as phantomjs from 'phantomjs';
+// const mocha = require('mocha');
+// const expect = require('chai').expect;
+
+// // import Server from '../../src/server/Server';
+// const Server = require('../../server/server/Server');
+
+// mocha.describe('Client', function() {
+//   before(function() {
+//     this.server = new Server();
+//     this.server.start();
+//   });
+
+//   after(function() {
+//     this.server.stop();
+//   });
+
+//   context('when loading the client', function() {
+//     it('should have a terminal interface');
+//     it('should have focus on the terminal input');
+//     it('should refocus on the terminal input when it loses focus');
+//     it('should accept commands from the terminal input');
+//     it('should echo entered commands to the terminal output');
+//   });
+// });
+
+phantom.exit();
