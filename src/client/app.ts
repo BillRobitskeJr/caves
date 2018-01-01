d33049b8242aa31e447e1efbdaba52f6006fc67f
Bill Robitske, Jr <bill.robitske.jr@gmail.com>
2018-01-01 13:55:48 -0600

    Cleanup - Restart

--- src/client/app.ts
+++
@@ -1,7 +0,0 @@
-import Terminal from './terminal/Terminal';
-
-window.addEventListener('load', () => {
-  const terminalElement = document.getElementById('terminal');
-  if (!terminalElement) return;
-  const terminal = new Terminal(terminalElement);
-});
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Server startup script
- */
-import Server from './server/Server';
-
-// Startup server on default port (3000)
-const server = new Server();
-server.start();
